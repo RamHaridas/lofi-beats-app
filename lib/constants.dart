74cbad2dabd8ad02d06bf224396a8fca95877722
Ram Haridas <43177945+RamHaridas@users.noreply.github.com>
2021-05-22 10:40:52 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const BASE_URL = "https://deezerdevs-deezer.p.rapidapi.com/";
-const API_KEY = "b88133c4ecmshc37539d81f77fe8p129603jsn79c139fd103a";
+const API_KEY = "YOUR_API_KEY";
 const google = 'assets/icons/google.svg';
 const bing = 'assets/icons/bing.svg';
 const yahoo = 'assets/icons/yahoo.svg';
@@ -13,10 +13,10 @@ const yahooSearch = "https://in.search.yahoo.com/search?p=";
 const duckSearch = "https://duckduckgo.com/html/?q=";
 
 const topImages =
-    "https://api.nasa.gov/planetary/apod?api_key=BURywN6AJo1TTDXDhQXycEQR3ASXHjccdtLGle2y&count=8";
+    "https://api.nasa.gov/planetary/apod?api_key=<YOUR_API_KEY>&count=8";
 
 const marsRoverImages =
-    "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&api_key=BURywN6AJo1TTDXDhQXycEQR3ASXHjccdtLGle2y";
+    "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&api_key=YOUR_API_KEY";
 const navBarTextStyle =
     TextStyle(fontWeight: FontWeight.bold, color: Colors.white);
 
